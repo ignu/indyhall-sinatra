220e506f36eb9e9d443fe21ca150806e1be8f555
Len Smith <len@barrison.com>
2012-01-14 13:44:10 -0500

    Moved to individual view files

--- app.rb
+++ app.rb
@@ -15,26 +15,3 @@ get "/tweets/:term" do |term|
   @tweets = Twitter.search term
   haml :tweets
 end
-
-__END__
-
-@@layout
-%html
-  %h1
-    Indy Hall Sinatra App
-  %hr
-    = yield
-  %i
-
-@@tweets
--@tweets.each do |tweet|
-  %img{:src => tweet['profile_image_url']}
-  %b
-    =tweet['from_user']
-  =tweet['text']
-  %hr
-
-@@hello
-%h1
-  Hello
-  =@name
